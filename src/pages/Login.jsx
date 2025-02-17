9fccd6a870bcb01fc7f3bb5b3b57076731b309dd
supriya <supriyarathor68@gmail.com>
2025-02-17 14:57:20 +0530

    setup

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='bg-yellow-600 p-12'>
+      <h1 className='text-blue-600'>suraj</h1>
+    </div>
+  )
+}
+
+export default Login
